a06cf340c87f9e7791f7cd9abe03e52e58c604a8
steve <stevepesce879@gmail.com>
2020-06-03 13:43:40 -0400

    started and finished program, forgot to commit

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,52 @@
 
+
+class CashRegister#==================================
+
+    @@all_total = []
+    @@all_item = []
+    attr_accessor \
+        :discount,
+        :total
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @@all_total = [@total] 
+        @@all_item = []
+    end
+
+    def add_item(title,price,quantity = 1)
+        quantity.times do
+            @@all_item << title
+        end
+        @total = @total + (quantity * price)
+        @@all_total << @total
+    end
+
+    def apply_discount
+        total_f = @total.to_f 
+        discount_f = @discount.to_f
+        new_total_f = total_f * ((100.0 - discount_f)/100.0)
+        @total = new_total_f.to_i
+        @@all_total << @total
+        if @discount != 0
+            "After the discount, the total comes to $#{@total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def items
+        @@all_item
+    end
+
+    def void_last_transaction
+        @@all_total.pop
+        @@all_item.pop
+        @total = @@all_total[@@all_total.length - 1]        
+    end
+    
+
+
+
+end#=================================================
